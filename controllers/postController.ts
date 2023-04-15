472c4323582f531ea595429d28cce2c7681b1e39
Bravo1B9 <brofe93@gmail.com>
2023-04-15 10:17:11 +0000

    Update create post functionality

--- controllers/postController.ts
+++ controllers/postController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import { postCollection } from "../db";
+import { ObjectId } from "mongodb";
+import { Post } from "../models/postModel";
 
 export const createPost = async (req: Request, res: Response) => {
-  const post = req.body;
+  const post: Post = {
+    title: req.body.title,
+    body: req.body.body,
+    tags: req.body.tags,
+    catagories: req.body.catagories,
+    author: new ObjectId(req.params.authorId),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  };
   await postCollection.insertOne(post);
   res.json({ msg: 'Post created' }).status(201);
 };
